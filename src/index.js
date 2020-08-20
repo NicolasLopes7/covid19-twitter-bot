4ff17274f96745866f61bc93f8ef949e4e4ad4e8
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2020-08-20 12:43:22 -0300

    Adding Set Interval

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ import {
     getImageContent,
     parseImageContentToTweet,
 } from "./services";
+
 import { getReportDate, isReport } from "./utils";
 import { TwitterClient } from "./lib";
 
-(async function a() {
+const sendedReports = [];
+
+setInterval(() => {
     const imgArray = await getImages();
     const dayImages = [];
     imgArray.forEach((img) => {
         if (
             isReport(img.alt) &&
             new Date().toISOString().slice(0, 10) ===
-                getReportDate(img.alt).toISOString().slice(0, 10)
+                getReportDate(img.alt).toISOString().slice(0, 10) &&
+            sendedReports.indexOf(img.alt) === -1
         )
             dayImages.push(img);
     });
 
     for (let i = 0; i < dayImages.length; i++) {
         const imageContent = await getImageContent(dayImages[i].src);
+        sendedReports.push(dayImages[i].alt);
         TwitterClient.tweet(parseImageContentToTweet(imageContent));
     }
-})();
+}, 10000)
